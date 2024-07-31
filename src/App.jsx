2916f6af2a7395c6731dc5c2981eb7569595dee8
Manoj <163978503+De-evil@users.noreply.github.com>
2024-07-31 23:57:09 +0530

    Add files via upload
    Package we need to run this piece of code
    
    npm create vite@4.0.0
    npm install bootstrap
    
    Successfully built a React-based todo app with Create, Read, Update, and Delete (CRUD) functionality.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TodoApp from "./Todo"
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 >To-Do</h1>
+      <TodoApp />
+    </div>
+  )
+}
+
+export default App
